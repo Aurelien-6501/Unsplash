707078aaa776739bd51c2380cfd976442a6611c8
Aurelien <aurelien.fillion.pro@gmail.com>
2024-12-04 08:47:27 +0100

    Initial Commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by Aurelien Fillion on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
